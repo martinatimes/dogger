fde16be900b10006384864e9f31e428da1fdf8fc
martinatimes <martinatime@proton.me>
2026-07-29 22:09:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+DEFAULT_SCAN_INTERVAL = 30
+
+TOKEN = "USDC"
+
+CSV_HEADERS = [
+    "timestamp",
+    "wallet",
+    "old_balance",
+    "new_balance",
+    "difference"
+]
+
+NOTIFICATION_THRESHOLD = 0.01
